61b0fb829ba7f058524a5f2a21b439c2308d97b4
vlad <vladprizma@mail.ru>
2022-07-15 00:18:22 +0600

    [+] core: GameInfo.class - дописаны сигнатуры методов

--- core/src/main/java/io/deeplay/GameInfo.java
+++ core/src/main/java/io/deeplay/GameInfo.java
@@ -11,21 +11,37 @@ public interface GameInfo {
      * @param side - сторона у которой мы получаем все возможные ходы
      * @return key - изначальная позиция фигуры, value - конечные позиции
      */
-    Multimap<Integer, Integer> getAllPossibleMoves(Side side);
+    Multimap<Integer, Integer> getAllPossibleMoves(int clientId, Side side);
 
     boolean isFinished(int clientId);
 
     GameStatus getGameStatus(int clientId);
 
     Side getWinner(int clientId);
+
     Side getTurnSide(int clientId);
+
     // TODO: getLastMoveType(int clientId); // рокировка, взятие на проходе и т. д.
 
     // TODO: getCurrentHistory(int clientId);
+
+    /**
+     * @param clientId
+     * @return возвращает строку в нотации FEN
+     */
     String getCurrentBoardState(int clientId);
 
-    // TODO: getUnsafeCells(int clientId, Side side);
-    // TODO: getOccupiedBy(int clientId, Side side);
-    // TODO: getSidesAllPiecesOfType(int clientId, Side side, Piece);
-    // TODO: getLostPiecesForSide(int clientId, Side side);
+    /**
+     * @param clientId
+     * @param side
+     * @return все фигуры на доске, для выбранной стороны
+     */
+    Multimap<PieceType, Integer> getSidesAlivePieces(int clientId, Side side);
+
+    /**
+     * @param clientId
+     * @param side
+     * @return все фигуры на доске, для выбранной стороны
+     */
+    Multimap<PieceType, Integer> getSidesLostPieces(int clientId, Side side);
 }
--- core/src/main/java/io/deeplay/Main.java
+++ core/src/main/java/io/deeplay/Main.java
@@ -1,5 +1,7 @@
+package io.deeplay;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("core works)");
+        System.out.println("core works");
     }
 }
