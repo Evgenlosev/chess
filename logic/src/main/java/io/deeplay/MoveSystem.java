18153418ebebfe3a3dc1549e8b1c001a76006215
vlad <vladprizma@mail.ru>
2022-07-15 13:56:20 +0600

    [+] - MoveSystem.java: интерфейс для движка шахмат

---
+++ logic/src/main/java/io/deeplay/MoveSystem.java
@@ -0,0 +1,16 @@
+package io.deeplay;
+
+import io.deeplay.model.ChessBoard;
+
+import java.util.List;
+
+public interface MoveSystem {
+
+
+    // TODO: нужен enum Side из core
+    //  List<Integer> getPawnMoves(ChessBoard board, Side side);
+
+    // TODO: нужен enum Side из core
+    //  List<Integer> getPawnMoves(ChessBoard board, Side side);
+
+}
