cf1a87e8528a7ba0160b2a03d9b0d24730385de4
vlad <vladprizma@mail.ru>
2022-07-15 14:20:03 +0600

    [+] - MoveSystem.java: добавлены все основные методы, пока закомментированы

--- logic/src/main/java/io/deeplay/MoveSystem.java
+++ logic/src/main/java/io/deeplay/MoveSystem.java
@@ -11,6 +11,33 @@ public interface MoveSystem {
     //  List<Integer> getPawnMoves(ChessBoard board, Side side);
 
     // TODO: нужен enum Side из core
-    //  List<Integer> getPawnMoves(ChessBoard board, Side side);
+    //  List<Integer> getKnightMoves(ChessBoard board, Side side);
+
+    // TODO: нужен enum Side из core
+    //  List<Integer> getBishopMoves(ChessBoard board, Side side);
+
+    // TODO: нужен enum Side из core
+    //  List<Integer> getRookMoves(ChessBoard board, Side side);
+
+    // TODO: нужен enum Side из core
+    //  List<Integer> getQueenMoves(ChessBoard board, Side side);
+
+    // TODO: нужен enum Side из core
+    //  List<Integer> getKingMoves(ChessBoard board, Side side);
+
+    // TODO: нужен enum Side из core
+    //  List<Integer> isKingUnderAttack(ChessBoard board, Side side);
+
+    // TODO: нужен enum Side из core
+    //  List<Integer> getAllPossibleMoves(ChessBoard board, Side side);
+
+    // TODO: нужен enum Side из core
+    //  boolean hasPossibleMoves(ChessBoard board, Side side); // getAllPossibleMoves(...) == 0
+
+    // TODO: нужен enum Side из core
+    //  boolean isKingCantMove(ChessBoard board, Side side); // getKingMoves(...) == 0
+
+    // TODO: нужен enum Side из core
+    //  boolean hasKingCoverMoves(ChessBoard board, Side side);
 
 }
