01f91a29ff275e2cea314747d249df0f7d6fbbc3
Evgenlosev <evgenlosev@yandex.ru>
2022-07-18 17:27:16 +0700

    [+] - interaction: добавлен класс Command и enam CommandType с основными видами команд

---
+++ interaction/src/main/java/io/deeplay/Command.java
@@ -0,0 +1,13 @@
+package io.deeplay;
+
+public abstract class Command {
+    private final CommandType commandType;
+
+    public Command(final CommandType commandType) {
+        this.commandType = commandType;
+    }
+
+    public CommandType getCommandType() {
+        return commandType;
+    }
+}
