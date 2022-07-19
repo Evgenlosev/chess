48d4deccb73c1571e52b53eecb5ed8a023a0755f
vlad <vladprizma@mail.ru>
2022-07-19 15:42:00 +0600

    [r] - MagicBoard.java: изменил комментарии x2

--- logic/src/main/java/io/deeplay/MagicBoard.java
+++ logic/src/main/java/io/deeplay/MagicBoard.java
@@ -3,20 +3,18 @@ package io.deeplay;
 public class MagicBoard {
 
     /**
-     * блокируемая маска содержащий все клетки, которые могут блокировать линейноходящие фигуры
+     * Блокируемая маска содержащая все клетки, которые могут блокировать линейноходящие фигуры
      * A bitboard containing all squares that can block a rook or a bishop.
      */
     public long blockerMask;
 
     /**
      * Сгенерированные доски передвижений
-     * The generated move boards.
      */
     public long[] moveBoards;
 
     /**
      * Предварительно вычисленное значение для того чтобы определить корректный индекс доски передвижений
-     * Precomputed value to determine the correct move board index.
      */
     public int shift;
 
