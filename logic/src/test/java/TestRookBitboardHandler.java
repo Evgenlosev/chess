6862709a725dc7095ed1a85085815a6779fd3db4
vlad <vladprizma@mail.ru>
2022-07-20 00:45:51 +0600

    [r] - test: поправки комментариев

--- logic/src/test/java/TestRookBitboardHandler.java
+++ logic/src/test/java/TestRookBitboardHandler.java
@@ -5,7 +5,7 @@ import org.junit.Test;
 import static org.junit.Assert.assertEquals;
 
 public class TestRookBitboardHandler {
-    // TODO: "закрепленная" за своим королем ладья должна ходить только по линии атаки "зажимающей" фигуры
+    // TODO: связанная за своим королем ладья должна ходить только по линии атаки связывающей фигуры
     //  по сути надо сделать && битборда атаки с битбородом атаки фигуры, угрожающей королю
 
     private long ALL_MASK = 0xFFFFFFFFFFFFFFFFL;
@@ -16,6 +16,19 @@ public class TestRookBitboardHandler {
         long occupied =     0b0010000000000000000000000000000000010001000000000010000100000000L;
         int rookPosition = 8;
 
+        /*
+        8| 0 0 0 0 0 1 0 0
+        7| 0 0 0 0 0 0 0 0
+        6| 0 0 0 0 0 0 0 0
+        5| 0 0 0 0 0 0 0 0
+        4| 0 0 0 1 1 0 0 0
+        3| 0 0 0 0 0 0 0 0
+        2|[1] 0 0 0 1 0 0 0
+        1| 0 0 0 0 0 0 0 0
+           _______________
+           a b c d e f g h
+         */
+
         long expectedBitboard = 0b0000000000000000000000000000000000000001000000010011111000000001L;
         assertEquals(BitUtils.getBitboardAsBinaryString(expectedBitboard),
                 BitUtils.getBitboardAsBinaryString(BitboardHandler.getRookMoves(rookPosition, occupied)));
@@ -25,6 +38,19 @@ public class TestRookBitboardHandler {
     public void testRookInMiddlePosition() {
         long occupied =     0b0010000000000000000000000000000000011000000000000001000100000000L;
         int rookPosition = 28;
+
+        /*
+        8| 0 0 0 0 0 1 0 0
+        7| 0 0 0 0 0 0 0 0
+        6| 0 0 0 0 0 0 0 0
+        5| 0 0 0 0 0 0 0 0
+        4| 0 0 0 1[1]0 0 0
+        3| 0 0 0 0 0 0 0 0
+        2| 1 0 0 0 1 0 0 0
+        1| 0 0 0 0 0 0 0 0
+           _______________
+           a b c d e f g h
+         */
 
         long expectedBitboard = 0b0001000000010000000100000001000011101000000100000001000000000000L;
         assertEquals(BitUtils.getBitboardAsBinaryString(expectedBitboard),
