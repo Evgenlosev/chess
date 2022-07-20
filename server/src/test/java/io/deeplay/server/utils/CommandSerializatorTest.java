988ce7e373748a127e0b2f6963db9517bd03a185
Evgenlosev <evgenlosev@yandex.ru>
2022-07-21 00:20:42 +0700

    [+] - server: CommandSerializatorTest.java - добавлены тесты для сериализатора.

---
+++ server/src/test/java/io/deeplay/server/utils/CommandSerializatorTest.java
@@ -0,0 +1,45 @@
+package io.deeplay.server.utils;
+
+import io.deeplay.interaction.Command;
+import io.deeplay.interaction.clientToServer.AuthRequest;
+import io.deeplay.interaction.clientToServer.MoveRequest;
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.io.IOException;
+
+import static org.junit.Assert.*;
+
+public class CommandSerializatorTest {
+
+    @Test
+    public void testSerializeCommand() throws IOException {
+        Command command = new AuthRequest("Bob");
+        byte[] bytes = CommandSerializator.serializeCommand(command);
+        Command result = CommandSerializator.deserializeByteArray(bytes);
+
+        Assert.assertEquals(command.getClass(), result.getClass());
+        Assert.assertEquals(command.getCommandType(), result.getCommandType());
+        Assert.assertEquals(((AuthRequest) command).getUserName(), ((AuthRequest) result).getUserName());
+    }
+
+    @Test
+    public void testDeserializeCommandException() {
+        String str = "Java";
+        byte[] bytes = str.getBytes();
+
+        Assert.assertThrows(IOException.class, () -> {
+            CommandSerializator.deserializeByteArray(bytes);
+        });
+    }
+
+    @Test
+    public void testParseByteArrayToCommand() throws Exception {
+        MoveRequest moveRequest = new MoveRequest(5,17);
+        String result = new String(CommandSerializator.serializeCommand(moveRequest));
+
+        Assert.assertTrue(result.contains("MOVE_REQUEST"));
+        Assert.assertTrue(result.contains("5"));
+        Assert.assertTrue(result.contains("17"));
+    }
+}
