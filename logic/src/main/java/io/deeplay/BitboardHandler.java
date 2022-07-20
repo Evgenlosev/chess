2264461db6e4588ce3badbc6c0fc292ecd01f8bf
vlad <vladprizma@mail.ru>
2022-07-20 23:02:44 +0600

    [+] - ChessBitboard.java: набросок методов получения ходов для коня и короля (не покрыты тестами).

--- logic/src/main/java/io/deeplay/BitboardHandler.java
+++ logic/src/main/java/io/deeplay/BitboardHandler.java
@@ -31,15 +31,46 @@ public class BitboardHandler {
         return movesInfo;
     }
 
-/*
     Set<MoveInfo> getKnightMoves(ChessBoard board, Coord from){
+        ChessBitboard chessBitboard = new ChessBitboard(board.getFenNotation(), from.getIndexAsOneDimension());
 
+        long allPossibleMoves = BitboardPatternsInitializer.knightMoveBitboards[from.getIndexAsOneDimension()];
+        Set<MoveInfo> movesInfo = new HashSet<>();
+        Figure figure = chessBitboard.getMySide() == Side.WHITE ? Figure.W_KNIGHT : Figure.B_KNIGHT;
+
+        long notMyPieces = ~chessBitboard.getMyPieces();
+        long notOpponentPieces = ~chessBitboard.getOpponentPieces();
+        for (long possibleMove : BitUtils.segregatePositions(allPossibleMoves)) {
+            if ((possibleMove & chessBitboard.getOpponentPieces()) != 0)
+                movesInfo.add(new MoveInfo(from, new Coord(Long.numberOfTrailingZeros(possibleMove)),
+                        MoveType.USUAL_ATTACK, figure));
+            if ((possibleMove & notMyPieces & notOpponentPieces) != 0)
+                movesInfo.add(new MoveInfo(from, new Coord(Long.numberOfTrailingZeros(possibleMove)),
+                        MoveType.USUAL_MOVE, figure));
+        }
+        return movesInfo;
     }
 
     Set<MoveInfo> getKingMoves(ChessBoard board, Coord from){
+        ChessBitboard chessBitboard = new ChessBitboard(board.getFenNotation(), from.getIndexAsOneDimension());
 
+        long allPossibleMoves = BitboardPatternsInitializer.kingMoveBitboards[from.getIndexAsOneDimension()];
+        Set<MoveInfo> movesInfo = new HashSet<>();
+        Figure figure = chessBitboard.getMySide() == Side.WHITE ? Figure.W_KING : Figure.B_KING;
+
+        long notMyPieces = ~chessBitboard.getMyPieces();
+        long notOpponentPieces = ~chessBitboard.getOpponentPieces();
+        for (long possibleMove : BitUtils.segregatePositions(allPossibleMoves)) {
+            if ((possibleMove & chessBitboard.getOpponentPieces()) != 0)
+                movesInfo.add(new MoveInfo(from, new Coord(Long.numberOfTrailingZeros(possibleMove)),
+                        MoveType.USUAL_ATTACK, figure));
+            if ((possibleMove & notMyPieces & notOpponentPieces) != 0)
+                movesInfo.add(new MoveInfo(from, new Coord(Long.numberOfTrailingZeros(possibleMove)),
+                        MoveType.USUAL_MOVE, figure));
+        }
+        return movesInfo;
     }
-*/
+
 
 /*
 // TODO:
