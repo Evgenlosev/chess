ec25d9ebeb096d9792579150b8d5c909ead4769d
Andrew <pagin.andrey.98@gmail.com>
2022-07-24 23:15:04 +0600

    [r] Board is 8*8 [+] Constructor to create board from FEN notation.

--- logic/src/main/java/io/deeplay/model/ChessBoard.java
+++ logic/src/main/java/io/deeplay/model/ChessBoard.java
@@ -1,61 +1,120 @@
 package io.deeplay.model;
 
+import ch.qos.logback.classic.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.Map;
+
+import static java.util.Map.entry;
 
 public class ChessBoard {
+    public static final int boardSize = 8;
+    private BoardCell[][] board = new BoardCell[boardSize][boardSize];
+    Logger logger = (Logger) LoggerFactory.getLogger(ChessBoard.class);
+    private ChessBoard previousChessBoard = null;
 
-    private BoardCell[][] board = new BoardCell[10][10];
+    Map<String, Figure> symbolsToFigure = Map.ofEntries(
+            entry("p", Figure.B_PAWN),
+            entry("r", Figure.B_ROOK),
+            entry("n", Figure.B_KNIGHT),
+            entry("b", Figure.B_BISHOP),
+            entry("q", Figure.B_QUEEN),
+            entry("k", Figure.B_KING),
+            entry("P", Figure.W_PAWN),
+            entry("R", Figure.W_ROOK),
+            entry("N", Figure.W_KNIGHT),
+            entry("B", Figure.W_BISHOP),
+            entry("Q", Figure.W_QUEEN),
+            entry("K", Figure.W_KING),
+            entry("1", Figure.NONE)
+    );
+    Map<Figure, String> figureToSymbol = Map.ofEntries(
+            entry(Figure.B_PAWN, "p"),
+            entry(Figure.B_ROOK, "r"),
+            entry(Figure.B_KNIGHT, "n"),
+            entry(Figure.B_BISHOP, "b"),
+            entry(Figure.B_QUEEN, "q"),
+            entry(Figure.B_KING, "k"),
+            entry(Figure.W_PAWN, "P"),
+            entry(Figure.W_ROOK, "R"),
+            entry(Figure.W_KNIGHT, "N"),
+            entry(Figure.W_BISHOP, "B"),
+            entry(Figure.W_QUEEN, "Q"),
+            entry(Figure.W_KING, "K"),
+            entry(Figure.NONE, "-")
+    );
 
     /**
      * Default constructor to create default board;
      */
     public ChessBoard() {
-        for (int i = 0; i < 10; i++) {
-            for (int j = 0; j < 10; j++) {
-                if (i < 1 || i > 8 || j < 1 || j > 8) {
-                    board[i][j] = new BoardCell(true, true, Figure.NONE);
-                }
-                if (i == 1 || j == 1 || i == 8 || j == 8) {
-                    board[i][j] = new BoardCell(false, true, Figure.NONE);
-                } else {
-                    board[i][j] = new BoardCell(false, false, Figure.NONE);
-                }
+        this("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1");
+    }
+
+    /**
+     * Constructor to create custom start board from FEN string
+     * Options like long pawn move, move number, will be ignored.
+     * @param fen FEN string.
+     */
+    public ChessBoard(final String fen) {
+//        throw new RuntimeException("Это ещё не было реализовано )-:");
+        if (fen.length() - fen.replace("/", "").length() != 7) {
+            throw new RuntimeException("Wrong FEN string");
+        }
+        String[] splitFiguresFromProperties = fen.split(" ", 2);
+        String unzippedFenWithoutProperties = unzipFen(splitFiguresFromProperties[0]).replace("/", "");
+        for (int i = 0; i < unzippedFenWithoutProperties.length(); i++) {
+            board[7 - i / 8][7 - i % 8] = new BoardCell(
+                    symbolsToFigure.get(String.valueOf(unzippedFenWithoutProperties.charAt(i))));
+        }
+    }
+
+    public String unzipFen(String fen) {
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < fen.length(); i++) {
+            if (String.valueOf(fen.charAt(i)).matches("[1-8]")) {
+                sb.append("1".repeat(Character.getNumericValue(fen.charAt(i))));
+            } else {
+                sb.append(fen.charAt(i));
             }
         }
-        board[1][1] = new BoardCell(false, true, Figure.W_ROOK);
-        board[1][2] = new BoardCell(false, true, Figure.W_KNIGHT);
-        board[1][3] = new BoardCell(false, true, Figure.W_BISHOP);
-        board[1][4] = new BoardCell(false, true, Figure.W_QUEEN);
-        board[1][5] = new BoardCell(false, true, Figure.W_KING);
-        board[1][6] = new BoardCell(false, true, Figure.W_BISHOP);
-        board[1][7] = new BoardCell(false, true, Figure.W_KNIGHT);
-        board[1][8] = new BoardCell(false, true, Figure.W_ROOK);
+        return sb.toString();
+    }
 
-        board[2][1] = new BoardCell(false, true, Figure.W_PAWN);
-        board[2][2] = new BoardCell(false, false, Figure.W_PAWN);
-        board[2][3] = new BoardCell(false, false, Figure.W_PAWN);
-        board[2][4] = new BoardCell(false, false, Figure.W_PAWN);
-        board[2][5] = new BoardCell(false, false, Figure.W_PAWN);
-        board[2][6] = new BoardCell(false, false, Figure.W_PAWN);
-        board[2][7] = new BoardCell(false, false, Figure.W_PAWN);
-        board[2][8] = new BoardCell(false, true, Figure.W_PAWN);
+    public String zipFen(final String fen) {
+        StringBuilder sb = new StringBuilder();
+        int emptyCellCounter = 0;
 
-        board[7][1] = new BoardCell(false, true, Figure.B_PAWN);
-        board[7][2] = new BoardCell(false, false, Figure.B_PAWN);
-        board[7][3] = new BoardCell(false, false, Figure.B_PAWN);
-        board[7][4] = new BoardCell(false, false, Figure.B_PAWN);
-        board[7][5] = new BoardCell(false, false, Figure.B_PAWN);
-        board[7][6] = new BoardCell(false, false, Figure.B_PAWN);
-        board[7][7] = new BoardCell(false, false, Figure.B_PAWN);
-        board[7][8] = new BoardCell(false, true, Figure.B_PAWN);
+        for (int i = 0; i < fen.length(); i++) {
+            if (fen.charAt(i) == '1') {
+                emptyCellCounter++;
+                continue;
+            }
+            if (emptyCellCounter > 0) {
+                sb.append(emptyCellCounter);
+                emptyCellCounter = 0;
+            }
+            sb.append(fen.charAt(i));
+        }
+        if (emptyCellCounter > 0) {
+            sb.append(emptyCellCounter);
+        }
+        return sb.toString();
+    }
 
-        board[8][1] = new BoardCell(false, true, Figure.B_ROOK);
-        board[8][2] = new BoardCell(false, true, Figure.B_KNIGHT);
-        board[8][3] = new BoardCell(false, true, Figure.B_BISHOP);
-        board[8][4] = new BoardCell(false, true, Figure.B_QUEEN);
-        board[8][5] = new BoardCell(false, true, Figure.B_KING);
-        board[8][6] = new BoardCell(false, true, Figure.B_BISHOP);
-        board[8][7] = new BoardCell(false, true, Figure.B_KNIGHT);
-        board[8][8] = new BoardCell(false, true, Figure.B_ROOK);
+    public void updateBoard(MoveInfo moveInfo) {
+
+        try {
+            previousChessBoard = (ChessBoard) this.clone();
+        } catch (CloneNotSupportedException e) {
+            throw new RuntimeException(e);
+        }
+        // TODO:: Сделать перемещение фигуры.
+    }
+
+    public String getFEN() {
+        return "88005553535";
+        // TODO:: Получить из массива строку фен, с указанием очередности хода, номером хода, количеством ходов без взятия фигур.
     }
 
     public BoardCell[][] getBoard() {
@@ -70,53 +129,18 @@ public class ChessBoard {
     public String toString() {
         StringBuilder sb = new StringBuilder();
 
-        for (int i = 1; i < 9; i++) {
-            for (int j = 1; j < 9; j++) {
-                switch (board[i][j].getFigure()) {
-                    case NONE:
-                        sb.append("  ");
-                        break;
-                    case B_KING:
-                        sb.append("k ");
-                        break;
-                    case B_BISHOP:
-                        sb.append("b ");
-                        break;
-                    case B_KNIGHT:
-                        sb.append("n ");
-                        break;
-                    case B_ROOK:
-                        sb.append("r ");
-                        break;
-                    case B_PAWN:
-                        sb.append("p ");
-                        break;
-                    case B_QUEEN:
-                        sb.append("q ");
-                        break;
-                    case W_KING:
-                        sb.append("K ");
-                        break;
-                    case W_BISHOP:
-                        sb.append("B ");
-                        break;
-                    case W_KNIGHT:
-                        sb.append("N ");
-                        break;
-                    case W_ROOK:
-                        sb.append("R ");
-                        break;
-                    case W_PAWN:
-                        sb.append("P ");
-                        break;
-                    case W_QUEEN:
-                        sb.append("Q ");
-                        break;
-                }
+        for (int i = 0; i < boardSize; i++) {
+            for (int j = 0; j < boardSize; j++) {
+                sb.append(figureToSymbol.get(board[i][j].getFigure())).append(" ");
             }
             sb.append("\n");
         }
 
         return sb.reverse().toString();
+    }
+
+    @Override
+    protected Object clone() throws CloneNotSupportedException {
+        return super.clone();
     }
 }
