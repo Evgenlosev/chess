f931cc5003ae67edce7b45221b12434751cb804f
Evgenlosev <evgenlosev@yandex.ru>
2022-07-26 12:06:48 +0700

    [*] - server: ChessNettyClient.java - добавлен список активных клиентов.

--- server/src/main/java/io/deeplay/server/ChessNettyServer.java
+++ server/src/main/java/io/deeplay/server/ChessNettyServer.java
@@ -13,14 +13,17 @@ import io.netty.channel.socket.nio.NioServerSocketChannel;
 import org.slf4j.LoggerFactory;
 import ch.qos.logback.classic.Logger;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class ChessNettyServer {
     private static final int PORT = 8189;
     private static final String PROTOCOL_VERSION = "1.0";
     private static final Logger logger = (Logger) LoggerFactory.getLogger(ChessNettyServer.class);
-
+    private static final Map<Integer, String> activeClients = new HashMap<>();
     public void run() throws Exception {
         //Пул потоков для обработки подключений клиентов
-        EventLoopGroup bossGroup = new NioEventLoopGroup();
+        EventLoopGroup bossGroup = new NioEventLoopGroup(1);
         //Пул потоков для обработки сетевых сообщений
         EventLoopGroup workerGroup = new NioEventLoopGroup();
         try {
@@ -56,6 +59,18 @@ public class ChessNettyServer {
 
     public static String getProtocolVersion() {
         return PROTOCOL_VERSION;
+    }
+
+    public static Integer addClient(String userName) {
+        if (activeClients.size() == 0) {
+            activeClients.put(1, userName);
+            return 1;
+        }
+        if (activeClients.containsValue(userName)) {
+            return 0;
+        }
+        activeClients.put(activeClients.size() + 1, userName);
+        return activeClients.size();
     }
 
     public static void main(String[] args) throws Exception {
