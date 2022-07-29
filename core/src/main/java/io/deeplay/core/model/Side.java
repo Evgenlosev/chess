0f0a90d00f0fc6166e2007da005627d3167328ff
Evgenlosev <evgenlosev@yandex.ru>
2022-07-29 15:05:51 +0700

    [*] - удалил toString().

--- core/src/main/java/io/deeplay/core/model/Side.java
+++ core/src/main/java/io/deeplay/core/model/Side.java
@@ -1,18 +1,11 @@
 package io.deeplay.core.model;
 
 public enum Side {
-    WHITE(),
-    BLACK();
+    WHITE,
+    BLACK;
 
     public static Side otherSide(final Side side){
         return side == WHITE ? BLACK : WHITE;
     }
 
-    @Override
-    public String toString() {
-        if (this == Side.WHITE) {
-            return "Белые";
-        }
-        return "Черные";
-    }
 }
