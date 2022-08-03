f34d7b0b0b498135b64512b04ce44890734b61e2
Andrew <pagin.andrey.98@gmail.com>
2022-08-03 16:34:35 +0600

    [+] finished pawn promotion, game finishes by rules.

--- core/src/main/java/io/deeplay/core/Main.java
+++ core/src/main/java/io/deeplay/core/Main.java
@@ -1,11 +1,13 @@
 package io.deeplay.core;
 
+import io.deeplay.core.console.BoardDrawer;
 import io.deeplay.core.model.Side;
 import io.deeplay.core.player.RandomBot;
 
 public class Main {
-    public static void main(final String[] args) {
-        SelfPlay selfPlay = new SelfPlay(new RandomBot(Side.WHITE), new RandomBot(Side.BLACK));
+    public static void main(String[] args) {
+        BoardDrawer.draw(null);
+        SelfPlay selfPlay = new SelfPlay(new RandomBot(Side.WHITE, 0), new RandomBot(Side.BLACK, 1));
         selfPlay.play();
     }
 }
