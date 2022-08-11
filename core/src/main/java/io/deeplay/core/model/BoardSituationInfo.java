7c88e3b06933d53b8866755f22db7127fcc9e5c4
vlad <vladprizma@mail.ru>
2022-08-11 16:47:07 +0600

    [+] - core: BoardSituationInfo.java - теперь хранит все возможные ходы.

--- core/src/main/java/io/deeplay/core/model/BoardSituationInfo.java
+++ core/src/main/java/io/deeplay/core/model/BoardSituationInfo.java
@@ -1,5 +1,7 @@
 package io.deeplay.core.model;
 
+import java.util.Set;
+
 /**
  * Содержит информацию о ситуации на доске относительно ходящей стороны.
  */
@@ -9,12 +11,18 @@ public class BoardSituationInfo {
     private final boolean isMate;
     private final boolean isStalemate;
     private final boolean isDrawByPieceShortage;
+    private final Set<MoveInfo> moveInfoSet;
 
-    public BoardSituationInfo(boolean isCheck, boolean isMate, boolean isStalemate, boolean isDrawByPieceShortage) {
+    public BoardSituationInfo(final boolean isCheck,
+                              final boolean isMate,
+                              final boolean isStalemate,
+                              final boolean isDrawByPieceShortage,
+                              final Set<MoveInfo> moveInfoSet) {
         this.isCheck = isCheck;
         this.isMate = isMate;
         this.isStalemate = isStalemate;
         this.isDrawByPieceShortage = isDrawByPieceShortage;
+        this.moveInfoSet = moveInfoSet;
     }
 
     public boolean isCheck() {
@@ -31,5 +39,9 @@ public class BoardSituationInfo {
 
     public boolean isDrawByPieceShortage() {
         return isDrawByPieceShortage;
+    }
+
+    public Set<MoveInfo> getMoveInfoSet() {
+        return moveInfoSet;
     }
 }
