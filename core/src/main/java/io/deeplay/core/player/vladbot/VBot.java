ff8dddc27d953974f05e4c48fadd0cd1062ac524
vlad <vladprizma@mail.ru>
2022-08-11 13:15:00 +0600

    [+] - core: Evaluation.java - интерфейс функции полезности. Функция полезности внедрена в бота.

--- core/src/main/java/io/deeplay/core/player/vladbot/VBot.java
+++ core/src/main/java/io/deeplay/core/player/vladbot/VBot.java
@@ -1,6 +1,7 @@
 package io.deeplay.core.player.vladbot;
 
 import ch.qos.logback.classic.Logger;
+import io.deeplay.core.evaluation.Evaluation;
 import io.deeplay.core.model.GameInfo;
 import io.deeplay.core.model.MoveInfo;
 import io.deeplay.core.model.Side;
@@ -10,8 +11,16 @@ import org.slf4j.LoggerFactory;
 public abstract class VBot extends Player {
     private static final Logger LOGGER = (Logger) LoggerFactory.getLogger(VBot.class);
 
-    public VBot(Side side) {
+    private final Evaluation evaluation;
+
+    public VBot(Side side, Evaluation evaluation) {
         super(side);
+        this.evaluation = evaluation;
+        LOGGER.info("Для {} установлена оценочная функция - {}", this, evaluation);
+    }
+
+    public Evaluation getEvaluation() {
+        return evaluation;
     }
 
     @Override
