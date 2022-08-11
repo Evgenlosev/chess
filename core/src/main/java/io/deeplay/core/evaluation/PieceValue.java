96a682bb3d790fc925b90317280fcd0aab88d164
vlad <vladprizma@mail.ru>
2022-08-11 13:25:39 +0600

    [s] - core: PieceValue.java - javadoc.

--- core/src/main/java/io/deeplay/core/evaluation/PieceValue.java
+++ core/src/main/java/io/deeplay/core/evaluation/PieceValue.java
@@ -3,6 +3,10 @@ package io.deeplay.core.evaluation;
 import io.deeplay.core.model.ChessBoard;
 import io.deeplay.core.model.Side;
 
+/**
+ * Функция оценки основанная только на ценности фигур.
+ * Данная оценка плоха тем, что не развивает фигуры, поэтому теряет большинство потенциала.
+ */
 public class PieceValue implements Evaluation {
 
     @Override
