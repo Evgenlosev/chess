c662f00e8ce2231c554d2f7d86115ddd7a46edc2
vlad <vladprizma@mail.ru>
2022-08-15 23:59:44 +0600

    [*] - core: SelfPlay.java - добавлена возможность играть заданное количество партий подряд, возможность включить режим сбора статистики, а так же геттеры для, необходимых статистике, полей.

--- core/src/main/java/io/deeplay/core/SelfPlay.java
+++ core/src/main/java/io/deeplay/core/SelfPlay.java
@@ -1,12 +1,12 @@
 package io.deeplay.core;
 
 import ch.qos.logback.classic.Logger;
-import io.deeplay.core.console.BoardDrawer;
 import io.deeplay.core.listener.GameInfoGroup;
 import io.deeplay.core.model.GameInfo;
 import io.deeplay.core.model.MoveInfo;
 import io.deeplay.core.model.Side;
 import io.deeplay.core.player.Player;
+import io.deeplay.core.statistics.Statistics;
 import org.slf4j.LoggerFactory;
 
 
@@ -18,7 +18,16 @@ public class SelfPlay {
     private final GameInfoGroup gameInfoGroup;
     private Player currentPlayerToMove;
 
-    public SelfPlay(final Player firstPlayer, final Player secondPlayer, final GameInfo gameInfo) {
+    /**
+     * Количество партий, которые оба игрока будут играть подряд(не меняя стороны).
+     */
+    private final int gamesAmount;
+
+    public SelfPlay(final Player firstPlayer,
+                    final Player secondPlayer,
+                    final GameInfo gameInfo,
+                    final int gamesAmount,
+                    final boolean gatherStatistics) {
         if (firstPlayer.getSide() == secondPlayer.getSide()) {
             throw new IllegalArgumentException("Соперники не могут играть одним цветом");
         }
@@ -34,6 +43,23 @@ public class SelfPlay {
         this.gameInfoGroup = new GameInfoGroup(gameInfo);
         gameInfoGroup.addListener(firstPlayer);
         gameInfoGroup.addListener(secondPlayer);
+        this.gamesAmount = gamesAmount;
+        if (gatherStatistics) {
+            gameInfoGroup.addListener(new Statistics(this));
+        }
+    }
+
+    public SelfPlay(final Player firstPlayer,
+                    final Player secondPlayer,
+                    final GameInfo gameInfo) {
+        this(firstPlayer, secondPlayer, gameInfo, 1, false);
+    }
+
+    public SelfPlay(final Player firstPlayer,
+                    final Player secondPlayer,
+                    final int gamesAmount,
+                    final boolean gatherStatistics) {
+        this(firstPlayer, secondPlayer, new GameInfo(), gamesAmount, gatherStatistics);
     }
 
     public SelfPlay(final Player firstPlayer, final Player secondPlayer) {
@@ -56,20 +82,41 @@ public class SelfPlay {
         LOGGER.info("{} присоединился к партии за белых", firstPlayer);
         gameInfoGroup.playerSeated(secondPlayer.getSide());
         LOGGER.info("{} присоединился к партии за черных", secondPlayer);
-        gameInfoGroup.gameStarted();
-        LOGGER.info("Партия началась");
-        //Пока игра не закончена, рассылаем всем слушателям ходы игроков
-        while (gameInfo.isGameOver()) {
-            BoardDrawer.draw(gameInfo.getFenBoard());
-            LOGGER.info("Ходят {}", currentPlayerToMove.getSide().getDescription());
-            final MoveInfo moveInfo = currentPlayerToMove.getAnswer(gameInfo);
-            gameInfoGroup.playerActed(currentPlayerToMove.getSide(), moveInfo);
-            LOGGER.info("{} совершили ход: {}", currentPlayerToMove.getSide().getDescription(), moveInfo.toString());
-            LOGGER.info("\n" + gameInfo.getBoard().toString());
-            changeCurrentPlayerToMove();
+        int countGamesAmount = 0;
+        while (countGamesAmount++ < gamesAmount) {
+            gameInfoGroup.gameStarted();
+            LOGGER.info("Партия началась");
+            //Пока игра не закончена, рассылаем всем слушателям ходы игроков
+            while (gameInfo.isGameOver()) {
+                // BoardDrawer.draw(gameInfo.getFenBoard());
+                LOGGER.info("Ходят {}", currentPlayerToMove.getSide().getDescription());
+                final MoveInfo moveInfo = currentPlayerToMove.getAnswer(gameInfo);
+                gameInfoGroup.playerActed(currentPlayerToMove.getSide(), moveInfo);
+                LOGGER.info("{} совершили ход: {}", currentPlayerToMove.getSide().getDescription(), moveInfo.toString());
+                LOGGER.info("\n" + gameInfo.getBoard().toString());
+                changeCurrentPlayerToMove();
+            }
+            // BoardDrawer.draw(gameInfo.getFenBoard());
+            gameInfoGroup.gameOver();
+            LOGGER.info("Игра закончена. {}", gameInfo.getGameStatus().getMessage());
+            if (countGamesAmount < gamesAmount)
+                gameInfo.restartGame();
         }
-        BoardDrawer.draw(gameInfo.getFenBoard());
-        gameInfoGroup.gameOver(gameInfo.getGameStatus());
-        LOGGER.info("Игра закончена. {}", gameInfo.getGameStatus().getMessage());
+    }
+
+    public Player getFirstPlayer() {
+        return firstPlayer;
+    }
+
+    public Player getSecondPlayer() {
+        return secondPlayer;
+    }
+
+    public int getGamesAmount() {
+        return gamesAmount;
+    }
+
+    public GameInfo getGameInfo() {
+        return gameInfo;
     }
 }
