0f20fa4217bfc12c0e2b344aba779262030a1c22
vlad <vladprizma@mail.ru>
2022-08-17 10:50:08 +0600

    [*] - core: EvaluationBot.java - изменил копирование из chessBoard на gameInfo(Стало еще медленнее? Вроде на 1 миллисекунду в ход).

--- core/src/main/java/io/deeplay/core/player/vladbot/EvaluationBot.java
+++ core/src/main/java/io/deeplay/core/player/vladbot/EvaluationBot.java
@@ -3,7 +3,9 @@ package io.deeplay.core.player.vladbot;
 import ch.qos.logback.classic.Logger;
 import io.deeplay.core.evaluation.Evaluation;
 import io.deeplay.core.evaluation.PeSTO;
-import io.deeplay.core.model.*;
+import io.deeplay.core.model.GameInfo;
+import io.deeplay.core.model.MoveInfo;
+import io.deeplay.core.model.Side;
 import org.slf4j.LoggerFactory;
 
 import java.util.Objects;
@@ -36,19 +38,16 @@ public class EvaluationBot extends VBot {
         MoveInfo bestMove = null;
         int bestScore = Integer.MIN_VALUE;
         int currentScore;
-        BoardSituationInfo currentMoveBoardSituation;
-        // Пат - лучше проигрышного хода (если лучший ход = проигрышному -> берем пат)
-        for (MoveInfo moveInfo : allMoves) {
-            final ChessBoard virtualChessBoard = new ChessBoard(gameInfo.getBoard());
-            virtualChessBoard.updateBoard(moveInfo);
-            currentScore = sideCoeff * getEvaluation().evaluateBoard(virtualChessBoard);
-            if (gameInfo.isMate(virtualChessBoard)) {
+        for (final MoveInfo moveInfo : allMoves) {
+            final GameInfo virtualGameInfo = gameInfo.copy(moveInfo); // копия с ходом
+            currentScore = sideCoeff * getEvaluation().evaluateBoard(virtualGameInfo.getBoard());
+            if (virtualGameInfo.isMate()) {
                 currentScore = Integer.MAX_VALUE;
             }
-            if (gameInfo.isStalemate(virtualChessBoard)
-                    || gameInfo.isDrawByPieceShortage(virtualChessBoard)
-                    || gameInfo.isThreefoldRepetition(virtualChessBoard)
-                    || gameInfo.isMovesWithoutAttackOrPawnMove(virtualChessBoard)) {
+            if (virtualGameInfo.isStalemate()
+                    || virtualGameInfo.isDrawByPieceShortage()
+                    || virtualGameInfo.isThreefoldRepetition()
+                    || virtualGameInfo.isMovesWithoutAttackOrPawnMove()) {
                 // Пат лучше, чем проигрыш, поэтому он ценнее на 1
                 currentScore = Integer.MIN_VALUE + 1;
             }
