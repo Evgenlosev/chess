736266fb9032693967f2667c122e70cc11789d84
Evgenlosev <evgenlosev@yandex.ru>
2022-08-19 04:40:31 +0700

    [s] - косметические правки.

--- interaction/src/main/java/io/deeplay/interaction/clientToServer/StartGameRequest.java
+++ interaction/src/main/java/io/deeplay/interaction/clientToServer/StartGameRequest.java
@@ -3,19 +3,20 @@ package io.deeplay.interaction.clientToServer;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonTypeName;
 import io.deeplay.core.model.Side;
+import io.deeplay.core.player.PlayerType;
 import io.deeplay.interaction.Command;
 import io.deeplay.interaction.CommandType;
 
 @JsonTypeName("StartGameRequest")
 public class StartGameRequest extends Command {
     private final Side side;
-    private final String enemyType;
+    private final PlayerType enemyType;
     public StartGameRequest() {
         this.side = null;
         this.enemyType = null;
     }
 
-    public StartGameRequest(final Side side, final String enemyType) {
+    public StartGameRequest(final Side side, final PlayerType enemyType) {
         this.side = side;
         this.enemyType = enemyType;
     }
@@ -24,7 +25,7 @@ public class StartGameRequest extends Command {
         return side;
     }
 
-    public String getEnemyType() {
+    public PlayerType getEnemyType() {
         return enemyType;
     }
 
