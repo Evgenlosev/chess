fc0b3907870ce6fbf6518d2a73bb6147e7e7a2eb
Andrew <pagin.andrey.98@gmail.com>
2022-08-25 16:50:22 +0600

    [+] bot names added

--- core/src/main/java/io/deeplay/core/player/PlayerFactory.java
+++ core/src/main/java/io/deeplay/core/player/PlayerFactory.java
@@ -1,7 +1,10 @@
 package io.deeplay.core.player;
 
 import io.deeplay.core.model.Side;
+import io.deeplay.core.player.chadBot.Minimax;
 import io.deeplay.core.player.evgenbot.EvgenBot;
+import io.deeplay.core.player.vladbot.namedbots.AlisaBot;
+import io.deeplay.core.player.vladbot.namedbots.EvaluationBot;
 
 public class PlayerFactory {
     public static Player createPlayer(final PlayerType playerType, final Side side) {
@@ -12,6 +15,12 @@ public class PlayerFactory {
                 return new RandomBot(side);
             case EVGEN_BOT:
                 return new EvgenBot(side);
+            case ALICE_BOT:
+                return new AlisaBot(side);
+            case CHAD_BOT:
+                return new Minimax(side, 5);
+            case EVALUATION_BOT:
+                return new EvaluationBot(side);
             default:
                 throw new UnsupportedOperationException("Unsupported player type - " + playerType);
         }
--- core/src/main/java/io/deeplay/core/player/PlayerType.java
+++ core/src/main/java/io/deeplay/core/player/PlayerType.java
@@ -3,7 +3,10 @@ package io.deeplay.core.player;
 public enum PlayerType {
     HUMAN("Человек"),
     EVGEN_BOT("Бот Евгения"),
-    RANDOM_BOT("Рандомный бот");
+    RANDOM_BOT("Рандомный бот"),
+    ALICE_BOT("Бот Алиса"),
+    CHAD_BOT("Бот Андрея"),
+    EVALUATION_BOT("Простой оценочный бот");
 
     private final String description;
 
